b64c0805be66213f99e2d99d9a8cff18cd8ae830
leonardopadilha <leonardopadilha28@gmail.com>
2023-07-20 07:01:01 -0300

    Refatoração do Projeto e core do sistema

--- modulo03/demo02-esmodules-internacionalization/src/index.js
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -1,33 +1,36 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readLine from 'readline'
 
-import database from './../database.json' assert {type: "json"}
+import database from './../database.json' //assert {type: "json"}
 import Person from "./person.js"
+import TerminalController from "./terminalController.js"
 
-DraftLog(console).addLineListener(process.stdin)
 const DEFAULT_LANG = "pt-BR"
+const STOP_TERM = ":q"
 
-const options = {
-    leftPad: 2,
-    columns: [
-        { field: "id", name: chalk.cyan("ID")},
-        { field: "vehicles", name: chalk.magenta("Vehicles")},
-        { field: "kmTraveled", name: chalk.cyan("Km Traveled")},
-        { field: "from", name: chalk.cyan("From")},
-        { field: "to", name: chalk.cyan("To")},
-    ]
+
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
+
+async function mainLoop() {
+    try {
+        const answer = await terminalController.question()
+
+        if (answer === STOP_TERM) {
+            terminalController.closeTerminal()
+            console.log('process finished')
+            return;
+        }
+
+        const person = Person.generateInstanceFromString(answer)
+        console.log('person', person.formatted(DEFAULT_LANG))
+        return mainLoop()
+   
+    } catch (error) {
+        console.log("DEU RUIM**", error)
+        return mainLoop()
+    }
 }
 
-const table = chalkTable(options, database.map(item => new Person(item).formatted(DEFAULT_LANG)))
-const print = console.draft(table)
+await mainLoop()
 
-const terminal = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 
-/* terminal.question('Qual é o seu nome?', msg => {
-    console.log('msg', msg.toString())
-}) */
+
