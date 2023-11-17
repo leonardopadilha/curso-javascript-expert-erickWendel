a5cbdb3249b2f42fd333a9f415807704b7c1b52a
leonardopadilha <leonardopadilha28@gmail.com>
2023-11-17 05:31:22 -0300

    Register with success

--- metodo-tajs/modulo03-conceitos-tajs/aula06-e2e-tests/test/api.e2e.test.js
+++ metodo-tajs/modulo03-conceitos-tajs/aula06-e2e-tests/test/api.e2e.test.js
@@ -33,4 +33,89 @@ describe('E2E Test Suite', () => {
                 return new Promise(resolve => server.close(resolve))
         })
     })
+
+    describe('E2E Tests for Server', () => {
+        let _testServer;
+        let _testServerAddress;
+
+        beforeAll(async () => {
+            process.env.NODE_ENV = 'test'
+            const { default: server } = await import('../src/index.js')
+            _testServer = server.listen();
+
+            await waitForServerStatus(_testServer)
+
+            const serverInfo = _testServer.address()
+            _testServerAddress = `http://localhost:${serverInfo.port}`
+        })
+
+        afterAll(done => _testServer.close(done))
+
+        it('should return 404 for unsupported routes', async () => {
+            const response = await fetch(`${_testServerAddress}/unsupported`, {
+                method: 'POST'
+            })
+            expect(response.status).toBe(404)
+        })
+
+        it('should return 404 unsupported method', async () => {
+            const response = await fetch(`${_testServerAddress}/persons`, {
+                method: 'PUT'
+            })
+            expect(response.status).toBe(404)
+        })
+
+        it('should return 400 and missing field message when body is invalid', 
+        async () => {
+            const invalidPerson = { name: 'Fulano da Silva'} // Missing CPF
+
+            const response = await fetch(`${_testServerAddress}/persons`, {
+                method: 'POST',
+                body: JSON.stringify(invalidPerson)
+            })
+            expect(response.status).toBe(400)
+            const data = await response.json()
+            expect(data.validationError).toEqual('cpf is required')
+        })
+
+        it('should return 400 and missing field message when body withoud name', 
+        async () => {
+            const invalidPerson = { cpf: '123.456.789-00'} // Missing Name
+            const response = await fetch(`${_testServerAddress}/persons`, {
+                method: 'POST',
+                body: JSON.stringify(invalidPerson)
+            })
+            expect(response.status).toBe(400)
+            const data = await response.json()
+            expect(data.validationError).toEqual('name is required')
+        })
+    })
+
+    describe('Register e2e', () => {
+        let _testServer;
+        let _testSeverAddress;
+
+        beforeAll(async () => {
+            process.env.NODE_ENV = 'test'
+            const { default: server } = await import('../src/index.js')
+            _testServer = server.listen();
+
+            const serverInfo = _testServer.address()
+            _testSeverAddress = `http://localhost:${serverInfo.port}`
+        })
+
+        afterAll(done => _testServer.close(done))
+
+        it('should return 200 for register with success', async () => {
+            const validPerson = {"name": "Xuxa da Silva","cpf": "12312312312"}
+
+            const response = await fetch(`${_testSeverAddress}/persons`, {
+                method: 'POST',
+                body: JSON.stringify(validPerson)
+            })
+            expect(response.status).toBe(200)
+            const data = await response.json()
+            expect(data.result).toEqual('ok')
+        })
+    })
 })
