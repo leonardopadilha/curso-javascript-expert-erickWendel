a56d886bddff1ac39be18aa92eda805c185065c6
leonardopadilha <leonardopadilha28@gmail.com>
2024-01-16 06:26:21 -0300

    Adapter

---
+++ modulo07/structural/05-adapter/src/business/adapters/rickAndMortyBRLAdapter.js
@@ -0,0 +1,8 @@
+import RickAndMortyBRL from "../integrations/rickAndMortyBRL.js";
+
+export default class RickAndMortyBRLAdapter {
+    static async getCharacters() {
+        return RickAndMortyBRL.getCharactersFromJSON()
+    }
+}
+
---
+++ modulo07/structural/05-adapter/src/business/adapters/rickAndMortyUSAAdapter.js
@@ -0,0 +1,8 @@
+import RickAndMortyUSA from "../integrations/rickAndMortyUSA.js";
+
+export default class RickAndMortyUSAAdapter {
+    static async getCharacters() {
+        return RickAndMortyUSA.getCharactersFromXML()
+    }
+}
+
